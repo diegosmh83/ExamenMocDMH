488fd6f0c5b14467cb48b42fc310a043980bb07d
Moreno <diegosmh83@educastur.es>
2025-11-21 17:30:34 +0100

    Initial commit

---
+++ src/main/java/com/example/ExamenMocDMH/ExamenMocDmhApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocDMH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocDmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocDmhApplication.class, args);
+	}
+
+}
